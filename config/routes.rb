89e9e35fb78f0636d7d89edc187b0851ea8fa2e8
Darren <darren@darrens-mbp.myfiosgateway.com>
2021-09-13 14:22:50 -0400

    react deploy ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :cars, only: [:index, :show, :create, :update, :destroy]
   resources :prices, only: [:index]
   resources :tickets, only: [:index, :show, :create, :destroy]
+  
 
-
-
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
